6971031a52bf3639186548ab4c231939cfe1cc64
hoas22 <henry.olmer@gmail.com>
2023-01-09 21:29:13 -0500

    first commit

---
+++ DESAFIOS/javascript/01_fundamentos/app.js
@@ -0,0 +1,35 @@
+
+// console.log('Hola mundo')
+
+/*
+*/
+let a = 20;
+var b = 30;
+let c = 40;
+let d = 50;
+let e = 'Hola'
+let x = a + b;
+
+const saludo = e
+
+
+// let a = 20, b = 30, c = 40, d = 50, x = a + b; 
+
+// formas de impresion en consola
+/*
+console.log(a);
+console.warn(b);
+console.error(c);
+console.info(c);
+*/
+
+/* metodo es una función que se encuentra dentro de un objeto
+console es el obejto
+log es el metodo
+ */
+console.log('%c Mis variables','color:blue; font-weight: bold');
+console.table({a, b, c, d, x});
+console.log({a})
+
+
+
